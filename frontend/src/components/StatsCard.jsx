af99dcb26fe7f278e8e5903ae375c57353864e6f
Patience Rutayisire <ivanrutayisire@gmail.com>
2025-12-26 12:52:16 +0000

    updated frontend

--- frontend/src/components/StatsCard.jsx
+++
@@ -1,8 +0,0 @@
-const StatsCard = ({ title, value }) => (
-  <div className="bg-white p-4 rounded shadow">
-    <p className="text-gray-500">{title}</p>
-    <p className="text-xl font-bold">{value}</p>
-  </div>
-);
-
-export default StatsCard;
